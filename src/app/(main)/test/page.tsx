af5fcf320400a3bae7214d8c18016fa030e9712e
Malik Lahlou <71925280+bimocd@users.noreply.github.com>
2025-11-28 20:04:41 +0100

    🗑️ Deleted test route & page

--- src/app/(main)/test/page.tsx
+++ src/app/(main)/test/page.tsx
@@ -1,6 +1,15 @@
 "use client";
-import Space from "@/cpn/widgets/space";
+import { inspect } from "node:util";
+import { useEffect, useState } from "react";
+import { fetchAPICorrect } from "@/app/api/r/[id]/correct/route";
 
 export default function TestPage() {
-  return <div className="size-full flex"><Space /></div>
+  const [text, setText] = useState("____");
+  useEffect(() => {
+    setText("____");
+    fetchAPICorrect("factorial", [3], {
+      n: 6
+    }).then((r) => setText(inspect(r, { depth: 3 })));
+  }, []);
+  return <>{text}</>;
 }
--- src/app/api/test/route.ts
+++
@@ -1,7 +0,0 @@
-import { resourceHandler } from "@/lib/resources";
-import { SuccessResponse } from "../helpers";
-
-export async function GET() {
-  const res = await resourceHandler.fetch("power");
-  return SuccessResponse({ res: res.generateRandomSeed() });
-}
