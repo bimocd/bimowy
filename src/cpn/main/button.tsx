3cdf4df6c30576e5fbd378734c99439b7cbbdacb
Malik Lahlou <71925280+bimocd@users.noreply.github.com>
2025-11-29 05:23:06 +0100

    ♻️ Random refactoring

--- src/cpn/main/button.tsx
+++ src/cpn/main/button.tsx
@@ -1,4 +1,3 @@
-import type { ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 import { Button as ShadcnButton } from "../ui/button";
 
@@ -9,24 +8,16 @@ export function Button({
   children,
   className,
   variant = "default",
-}: {
-  id?: string;
-  onClick?: () => void;
-  disabled?: boolean;
-  children: ReactNode;
-  className?: string;
-  variant?: Parameters<typeof ShadcnButton>["0"]["variant"];
-}) {
+}: Parameters<typeof ShadcnButton>[0]) {
   return (
     <ShadcnButton
-      {...{ id }}
+      {...{ disabled, onClick, variant, id }}
       className={twMerge(
         `font-semibold cursor-pointer
     hover:scale-105 active:scale-95
     disabled:opacity-50 disabled:cursor-not-allowed`,
         className,
       )}
-      {...{ disabled, onClick, variant }}
     >
       {children}
     </ShadcnButton>
