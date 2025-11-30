16a14d85d86dfa7b60b4dda55fcd27508c4def5d
Malik Lahlou <71925280+bimocd@users.noreply.github.com>
2025-11-30 15:02:13 +0100

    🐛 Fixed basic article config

--- src/lib/resources/builders/article.ts
+++ src/lib/resources/builders/article.ts
@@ -1,9 +1,12 @@
 import { BaseResourceBuilder, type BaseResourceConfig } from "./base";
 import type { BSTUINode } from "./bst/nodes";
 
+type ArticleResourceConfig = Omit<BaseResourceConfig, "type"> & {
+  uiPlan: BSTUINode;
+};
 export class ArticleResourceBuilder extends BaseResourceBuilder {
   public uiPlan!: BSTUINode;
-  constructor(config: Omit<BaseResourceConfig, "type">) {
+  constructor(config: ArticleResourceConfig) {
     super({ ...config, type: "article" });
   }
 }
--- src/lib/resources/list/matrix.ts
+++ src/lib/resources/list/matrix.ts
@@ -1,3 +1,4 @@
+import { $ } from "../builders";
 import { ArticleResourceBuilder } from "../builders/article";
 
 export default new ArticleResourceBuilder({
@@ -5,4 +6,13 @@ export default new ArticleResourceBuilder({
   name: "Matrices 101",
   tags: ["linear-algebra"],
   beta: true,
+  uiPlan: $.prgh([
+    $.text(
+      "This is a matrix: \\(\\begin{bmatrix}" +
+        "\n1 & 2 & 3\\" +
+        "\na & b & c" +
+        "\nend{bmatrix}\\)",
+      { latex: true },
+    ),
+  ]),
 });
