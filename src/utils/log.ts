6aee7a2ab924c45aa7420ff6060e1dc2211e5eb3
Malik Lahlou <71925280+bimocd@users.noreply.github.com>
2026-02-18 17:37:37 +0000

    ✨ feat: custom log function

---
+++ src/utils/log.ts
@@ -0,0 +1,34 @@
+import { inspect } from "node:util";
+
+const emojis = {
+	ok: "✅",
+	err: "❌",
+	yay: "🎉",
+	warn: "⚠️",
+	info: "🟦",
+	debug: "🐛",
+	skip: "⏭️",
+	prep: "⏳",
+	start: "🚀",
+	end: "🏁",
+	fail: "💥",
+	question: "❓",
+	delete: "🗑️ "
+} as const;
+
+type Emoji = (typeof emojis)[keyof typeof emojis];
+
+export function log(emoji: Emoji, message: unknown, level: number = 0) {
+	const now = new Date();
+	const formattedNow =
+		`${String(now.getMonth() + 1).padStart(2, "0")}` +
+		`/${String(now.getDate()).padStart(2, "0")}` +
+		` ${String(now.getHours()).padStart(2, "0")}` +
+		`:${String(now.getMinutes()).padStart(2, "0")}` +
+		`:${String(now.getSeconds()).padStart(2, "0")}`;
+	const correctMessage =
+		typeof message === "string"
+			? message
+			: inspect(message, { colors: true, numericSeparator: true });
+	console.log(`[${formattedNow}]${" ".repeat(level + 1)}${emoji} ${correctMessage}`);
+}
