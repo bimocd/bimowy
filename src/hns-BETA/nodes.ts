508768d700e865bd800465087d256a9fc5efcf48
Malik Lahlou <71925280+bimocd@users.noreply.github.com>
2026-02-19 13:27:00 +0000

    ♻️ re: Added helper types for composite nodes

--- src/hns-BETA/nodes.ts
+++ src/hns-BETA/nodes.ts
@@ -8,25 +8,22 @@ import z from "zod";
 	> NSCompositeNode // { _bsttype: "xxxxx", ... }
  */
 
+export type NSBaseCompositeNode<NSType extends string> = { _nstype: NSType }
+export type BSCompositeNodeProps<NSKey extends string> = { [key in NSKey]: NSNode}
+
+
 export type NSPrimitiveNode = z.infer<typeof NSPrimitiveNodeSchema>;
 export type NSListNode = NSNode[];
-export type NSIfNode = { _nstype: "if", condition: NSNode, success: NSNode, fail: NSNode };
+export type NSIfNode = NSBaseCompositeNode<"if"> & BSCompositeNodeProps<"condition" | "success" | "fail">
 
 export const NSPrimitiveNodeSchema = z.union([z.string(), z.number(), z.boolean(), z.null()]);
 export const NSListNodeSchema: z.ZodType<NSListNode> = z.array(z.lazy(() => NSNodeSchema));
 export const NSIfNodeSchema: z.ZodType<NSIfNode> = z.object({
 	_nstype: z.literal("if"),
-	get condition(){
-		return NSNodeSchema
-	},
-	get success(){
-		return NSNodeSchema
-	},
-	get fail(){
-		return NSNodeSchema
-	}
+	get condition(){ return NSNodeSchema },
+	get success(){ return NSNodeSchema },
+	get fail(){ return NSNodeSchema }
 })
-
 
 export type NSNode = NSPrimitiveNode | NSListNode | NSIfNode;
 
