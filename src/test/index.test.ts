5d28fab8920ea4b0d2280c830b53a5a26cd799d9
Malik Lahlou <71925280+bimocd@users.noreply.github.com>
2026-02-21 18:05:37 +0000

    🚚 move: moved the node system tests inside a dedicated folder

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,17 +1,1 @@
-import assert from "node:assert";
-import test from "node:test";
-import { executeNS } from "@/ns/execute";
-import { $ } from "@/ns/helpers";
-
-test("Node System", () => {
-	test("executeNS", () => {
-		const prgm1 = $.prgm([
-			$.varSet("a_age", 15),
-			$.varSet("b_age", 24),
-			$.varSet("is_b_major", $.fn("compare", [">=", $.varGet("a_age"), 18])),
-			$.varSet("is_a_major", $.fn("compare", [">=", $.varGet("b_age"), 18])),
-			$.return([$.varGet("is_a_major"), $.varGet("is_b_major")])
-		]);
-		return assert.deepStrictEqual(executeNS(prgm1), [true, false]);
-	});
-});
+import "./ns.test";
